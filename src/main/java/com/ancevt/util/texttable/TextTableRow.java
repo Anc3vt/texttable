2e8060b557eaecfd8c50f8a269337b0597b8b6c4
Ancevt <i@ancevt.ru>
2022-03-13 11:18:19 +0300

    change domain name to com

--- src/main/java/com/ancevt/util/texttable/TextTableRow.java
+++ src/main/java/com/ancevt/util/texttable/TextTableRow.java
@@ -15,7 +15,7 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package ru.ancevt.util.texttable;
+package com.ancevt.util.texttable;
 
 import lombok.Data;
 
