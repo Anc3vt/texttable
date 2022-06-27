81041e04bce5bb9f755db927638a1febfbaee7d3
Ancevt <i@ancevt.ru>
2022-06-27 22:23:08 +0300

    v0.0.1: removed redundant space when decor mode is off

--- src/main/java/com/ancevt/util/texttable/TextTable.java
+++ src/main/java/com/ancevt/util/texttable/TextTable.java
@@ -24,275 +24,274 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TextTable {
-	
-	private static final int DEFAULT_MAX_STRING_LENGTH = 100;
-	
-	public static void main(String[] args) {
+
+    private static final int DEFAULT_MAX_STRING_LENGTH = 100;
+
+    public static void main(String[] args) {
 /*
 		+----+--------------------+------------+------+------+
 		| id | name               | country_id | code | desc |
 		+----+--------------------+------------+------+------+
 		|  1 | Moscow             | 1          | MOS  | NULL |
 		|  2 | New-York           | 3          | NY   | NULL |
-		|  3 | Kiyv               | 2          | KI   | NULL |
+		|  3 | Kiev               | 2          | KI   | NULL |
 		|  6 | Washington         | 3          | WS   | NULL |
 		|  7 | Samara             | 1          | SM   | NULL |
 		+----+--------------------+------------+------+------+
 */
-		
-		final TextTable textTable = new TextTable("id", "name", "country_id", "code", "desc");
 
-		textTable.addRow(1, "Moscow",     1, "MOS", "NULL");
-		textTable.addRow(2, "New-York",   3, "NY",  "NULL");
-		textTable.addRow(3, "Kiyv",       2, "KI",  "NULL");
-		textTable.addRow(6, "Washington", 4, "WS",  "NULL");
-		textTable.addRow(7, "Samara",     1, "SM",  "NULL");
+        final TextTable textTable = new TextTable("id", "name", "country_id", "code", "desc");
 
-		System.out.println(textTable.render());
+        textTable.addRow(1, "Moscow", 1, "MOS", "NULL");
+        textTable.addRow(2, "New-York", 3, "NY", "NULL");
+        textTable.addRow(3, "Kiev", 2, "KI", "NULL");
+        textTable.addRow(6, "Washington", 4, "WS", "NULL");
+        textTable.addRow(7, "Samara", 1, "SM", "NULL");
 
-		System.out.println();
+        System.out.println(textTable.render());
 
-		final TextTable textTable2 = new TextTable("id", "name", "country_id", "code", "desc");
-		textTable2.setDecorEnabled(false);
+        System.out.println();
 
-		textTable2.addRow(1, "Moscow0",     1, "MOS", "NULL");
-		textTable2.addRow(2, "New-York",   3, "NY",  "NULL");
-		textTable2.addRow(3, "Kiyv",       2, "KI",  "NULL");
-		textTable2.addRow(6, "Washington", 4, "WS",  "NULL");
-		textTable2.addRow(7, "Samara0",     1, "SM",  "NULL");
+        final TextTable textTable2 = new TextTable("id", "name", "country_id", "code", "desc");
+        textTable2.setDecorEnabled(false);
 
-		System.out.println(textTable2.render());
-	}
+        textTable2.addRow(1, "Moscow0", 1, "MOS", "NULL");
+        textTable2.addRow(2, "New-York", 3, "NY", "NULL");
+        textTable2.addRow(3, "Kiev", 2, "KI", "NULL");
+        textTable2.addRow(6, "Washington", 4, "WS", "NULL");
+        textTable2.addRow(7, "Samara0", 1, "SM", "NULL");
 
-	@Getter
-	@Setter
-	private int maxStringLength;
+        System.out.println(textTable2.render());
+    }
 
-	@Getter
-	@Setter
-	private String[] columnNames;
-	
-	private final List<TextTableRow> rows;
+    @Getter
+    @Setter
+    private int maxStringLength;
 
-	private int[] columnSizes;
+    @Getter
+    @Setter
+    private String[] columnNames;
 
-	private boolean decorEnabled;
+    private final List<TextTableRow> rows;
 
-	public TextTable() {
-		rows = new ArrayList<>();
-		setMaxStringLength(DEFAULT_MAX_STRING_LENGTH);
-		decorEnabled = true;
-	}
+    private int[] columnSizes;
 
-	public TextTable(boolean decorEnabled, String... columnNames) {
-		this(columnNames);
-		this.decorEnabled = decorEnabled;
-	}
+    private boolean decorEnabled;
 
-	public TextTable(boolean decorEnabled) {
-		this();
-		this.decorEnabled = decorEnabled;
-	}
-	
-	public TextTable(String... columnNames) {
-		this();
-		setColumnNames(columnNames);
-	}
+    public TextTable() {
+        rows = new ArrayList<>();
+        setMaxStringLength(DEFAULT_MAX_STRING_LENGTH);
+        decorEnabled = true;
+    }
 
-	public void setDecorEnabled(boolean decorEnabled) {
-		this.decorEnabled = decorEnabled;
-	}
+    public TextTable(boolean decorEnabled, String... columnNames) {
+        this(columnNames);
+        this.decorEnabled = decorEnabled;
+    }
 
-	public boolean isDecorEnabled() {
-		return decorEnabled;
-	}
+    public TextTable(boolean decorEnabled) {
+        this();
+        this.decorEnabled = decorEnabled;
+    }
 
-	public void addRow(Object... cells) {
-		final TextTableRow row = new TextTableRow();
-		row.setData(cells);
-		rows.add(row);
-	}
-	
-	public void addKeyedRow(Object key, Object[] cells) {
-		final TextTableRow row = new TextTableRow();
-		row.setData(cells);
-		row.setKey(key);
-		rows.add(row);
-	}
-	
-	public int size() {
-		return rows.size();
-	}
-	
-	public Object[] getRow(Object key) {
-		for(final TextTableRow row : rows)
-			if(row.getKey() == key || key.equals(row.getKey()))
-				return row.getData();
-		
-		return new Object[0];
-	}
-	
-	public Object[] getRow(int index) {
-		return rows.get(index).getData();
-	}
-	
-	public void removeRow(Object key) {
-		for(final TextTableRow row : rows) {
-			if(row.getKey() == key || key.equals(row.getKey())) {
-				rows.remove(row);
-				break;
-			}
-		}
-	}
-	
-	public boolean hasRow(Object key) {
-		for(final TextTableRow row : rows)
-			if(row.getKey() == key || key.equals(row.getKey()))
-				return true;
-		
-		return false;
-	}
-	
-	public String render() {
-		final StringBuilder sb = new StringBuilder();
-		
-		detectColumnSizes();
-		
-		sb.append(columnNames != null ? renderHeader() : renderDecor());
+    public TextTable(String... columnNames) {
+        this();
+        setColumnNames(columnNames);
+    }
 
-		if(isDecorEnabled()) sb.append('\n');
-		
-		for(int i = 0; i < size(); i ++) {
-			sb.append(renderLine(getRow(i)));
-			sb.append('\n');
-		}
-		
-		sb.append(renderDecor());
-		
-		return sb.toString();
-	}
+    public void setDecorEnabled(boolean decorEnabled) {
+        this.decorEnabled = decorEnabled;
+    }
 
-	private String renderDecor() {
-		if(!decorEnabled) return "";
+    public boolean isDecorEnabled() {
+        return decorEnabled;
+    }
 
-		final StringBuilder sb = new StringBuilder();
-		
-		sb.append('+');
-		for (int size : columnSizes) {
-			for(int j = 0; j < size + 1; j ++) {
-				sb.append('-');
-			}
-			sb.append('+');
-		}
-		
-		return sb.toString();
-	}
-	
-	private String renderHeader() {
-		return renderDecor() +
-				(isDecorEnabled() ? '\n' : "") +
-				renderLine(columnNames) +
-				'\n' +
-				renderDecor();
-	}
-	
-	private String renderLine(Object[] cells) {
-		final StringBuilder sb = new StringBuilder();
+    public void addRow(Object... cells) {
+        final TextTableRow row = new TextTableRow();
+        row.setData(cells);
+        rows.add(row);
+    }
 
-		if(isDecorEnabled()) sb.append('|');
-		
-		for (int i = 0; i < cells.length; i++) {
-			if(i >= columnSizes.length) break;
-			
-			String n = String.valueOf(cells[i]);
-			if(n.length() > maxStringLength) {
-				n = n.substring(0, maxStringLength - 2) + "..";
-			}
-			
-			sb.append(' ');
-			sb.append(n);
-			final int spacesLeftToRender = getSpacesLeft(n, columnSizes[i]);
-			sb.append(spaces(spacesLeftToRender));
+    public void addKeyedRow(Object key, Object[] cells) {
+        final TextTableRow row = new TextTableRow();
+        row.setData(cells);
+        row.setKey(key);
+        rows.add(row);
+    }
+
+    public int size() {
+        return rows.size();
+    }
+
+    public Object[] getRow(Object key) {
+        for (final TextTableRow row : rows)
+            if (row.getKey() == key || key.equals(row.getKey()))
+                return row.getData();
+
+        return new Object[0];
+    }
+
+    public Object[] getRow(int index) {
+        return rows.get(index).getData();
+    }
+
+    public void removeRow(Object key) {
+        for (final TextTableRow row : rows) {
+            if (row.getKey() == key || key.equals(row.getKey())) {
+                rows.remove(row);
+                break;
+            }
+        }
+    }
+
+    public boolean hasRow(Object key) {
+        for (final TextTableRow row : rows)
+            if (row.getKey() == key || key.equals(row.getKey()))
+                return true;
+
+        return false;
+    }
+
+    public String render() {
+        final StringBuilder sb = new StringBuilder();
+
+        detectColumnSizes();
+
+        sb.append(columnNames != null ? renderHeader() : renderDecor());
+
+        if (isDecorEnabled()) sb.append('\n');
+
+        for (int i = 0; i < size(); i++) {
+            sb.append(renderLine(getRow(i)));
+            sb.append('\n');
+        }
+
+        sb.append(renderDecor());
+
+        return sb.toString();
+    }
+
+    private String renderDecor() {
+        if (!decorEnabled) return "";
+
+        final StringBuilder sb = new StringBuilder();
+
+        sb.append('+');
+        for (int size : columnSizes) {
+            for (int j = 0; j < size + 1; j++) {
+                sb.append('-');
+            }
+            sb.append('+');
+        }
+
+        return sb.toString();
+    }
+
+    private String renderHeader() {
+        return renderDecor() +
+                (isDecorEnabled() ? '\n' : "") +
+                renderLine(columnNames) +
+                '\n' +
+                renderDecor();
+    }
+
+    private String renderLine(Object[] cells) {
+        final StringBuilder sb = new StringBuilder();
+
+        if (isDecorEnabled()) sb.append('|');
+
+        for (int i = 0; i < cells.length; i++) {
+            if (i >= columnSizes.length) break;
+
+            String n = String.valueOf(cells[i]);
+            if (n.length() > maxStringLength) {
+                n = n.substring(0, maxStringLength - 2) + "..";
+            }
+
+            if (isDecorEnabled()) sb.append(' ');
+            sb.append(n);
+            final int spacesLeftToRender = getSpacesLeft(n, columnSizes[i]);
+            sb.append(spaces(spacesLeftToRender));
+
+            if (isDecorEnabled()) sb.append('|');
+        }
+
+        if (cells.length < columnSizes.length) {
+            final int difference = columnSizes.length - cells.length;
+
+            for (int i = 0; i < difference; i++) {
+                sb.append(spaces(columnSizes[i + cells.length] + 1));
+                sb.append('|');
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static int getSpacesLeft(String string, int size) {
+        return size - string.length();
+    }
+
+    private static String spaces(int count) {
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) sb.append(' ');
+        return sb.toString();
+    }
+
+    private void detectColumnSizes() {
+        int columnCount = 0;
+        if (columnNames != null) {
+            columnCount = columnNames.length;
+        } else {
+            int max = 0;
+            for (final TextTableRow row : rows) {
+                final Object[] cells = row.getData();
+                if (cells.length > max) max = cells.length;
+            }
+            columnCount = max;
+        }
+
+        this.columnSizes = new int[columnCount];
+
+        if (columnNames != null) {
+            for (String columnName : columnNames) {
+                for (int j = 0; j < columnName.length(); j++) {
+                    if (j >= columnSizes.length) break;
+
+                    String string = columnNames[j];
+
+                    if (string.length() > maxStringLength) {
+                        string = string.substring(0, maxStringLength);
+                    }
 
 
-			if(isDecorEnabled()) sb.append('|');
-		}
-		
-		if (cells.length < columnSizes.length) {
-			final int difference = columnSizes.length - cells.length;
-			
-			for(int i = 0; i < difference; i ++) {
-				sb.append(spaces(columnSizes[i + cells.length] + 1));
-				sb.append('|');
-			}
-		}
-		
-		return sb.toString();
-	}
-	
-	private static int getSpacesLeft(String string, int size) {
-		return size - string.length();
-	}
-	
-	private static String spaces(int count) {
-		final StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < count; i ++) sb.append(' ');
-		return sb.toString();
-	}
-	
-	private void detectColumnSizes() {
-		int columnCount = 0;
-		if (columnNames != null) {
-			columnCount = columnNames.length;
-		} else {
-			int max = 0;
-			for(final TextTableRow row : rows) {
-				final Object[] cells = row.getData();
-				if(cells.length > max) max = cells.length;
-			}
-			columnCount = max;
-		}
+                    final int stringLength = string.length();
 
-		this.columnSizes = new int[columnCount];
+                    if (stringLength > columnSizes[j])
+                        columnSizes[j] = stringLength + 1;
+                }
+            }
+        }
 
-		if(columnNames != null) {
-			for (String columnName : columnNames) {
-				for (int j = 0; j < columnName.length(); j++) {
-					if (j >= columnSizes.length) break;
+        for (int i = 0; i < size(); i++) {
+            final Object[] rowData = getRow(i);
 
-					String string = columnNames[j];
+            for (int j = 0; j < rowData.length; j++) {
+                if (j >= columnSizes.length) break;
 
-					if (string.length() > maxStringLength) {
-						string = string.substring(0, maxStringLength);
-					}
+                String string = String.valueOf(rowData[j]);
+                if (string.length() > maxStringLength) {
+                    string = string.substring(0, maxStringLength);
+                }
 
+                final int stringLength = string.length();
 
-					final int stringLength = string.length();
-
-					if (stringLength > columnSizes[j])
-						columnSizes[j] = stringLength + 1;
-				}
-			}
-		}
-		
-		for(int i = 0; i < size(); i ++) {
-			final Object[] rowData = getRow(i);
-			
-			for(int j = 0; j < rowData.length; j ++) {
-				if (j >= columnSizes.length) break;
-				
-				String string = String.valueOf(rowData[j]);
-				if (string.length() > maxStringLength) {
-					string = string.substring(0, maxStringLength);
-				}
-				
-				final int stringLength = string.length();
-				
-				if (stringLength > columnSizes[j])
-					columnSizes[j] = stringLength + 2;
-			}
-		}
-	}
+                if (stringLength > columnSizes[j])
+                    columnSizes[j] = stringLength + 2;
+            }
+        }
+    }
 
 }
 
